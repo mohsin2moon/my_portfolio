b27d37a234a090a059b498f56832a786ce6c117d
mohsin2moon <113849285+mohsin2moon@users.noreply.github.com>
2024-07-06 17:38:41 +0600

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,78 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.png";
+import solidity from "./tech/solidity.svg";
+import hardhat from "./tech/hardhat.svg";
+import eth from "./tech/eth.png";
+import t3 from "./tech/t3.png"
+import next from './tech/next.png'
+
+import teciz from "./company/teciz.png";
+import technyx from "./company/technyx.png";
+import qsols from './company/qsols.png'
+
+import project1 from "./project1.png";
+import project2 from "./project2.png";
+import Project4 from "./Project4.png";
+import project5 from "./project5.png";
+import Project6 from "./Project6.png"
+import Project7 from "./Project7.png"
+import Project8 from "./Project8.png"
+
+export {
+  logo,
+  hardhat,
+  eth,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  t3,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  next,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  teciz,
+  technyx,
+  qsols,
+  solidity,
+  project1,
+  project2,
+  Project4,
+  project5,
+  Project6,
+  Project7,
+  Project8
+};
